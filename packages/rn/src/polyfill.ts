4cdb42a52d64b47827c309149e9f67706b103c8a
Gao Sun <gao@silverhand.io>
2024-04-19 17:45:43 +0800

    chore: upgrade packages (#23)
    * chore: upgrade packages
    
    * refactor: fix lint errors
    
    * chore: add react-native to peer deps

--- packages/rn/src/polyfill.ts
+++ packages/rn/src/polyfill.ts
@@ -7,11 +7,7 @@
 /* eslint-disable @silverhand/fp/no-mutation, @typescript-eslint/no-unnecessary-condition */
 import { btoaPolyfill, atobPolyfill } from 'js-base64';
 
-if (!global.btoa) {
-  global.btoa = btoaPolyfill;
-}
+global.btoa ||= btoaPolyfill;
 
-if (!global.atob) {
-  global.atob = atobPolyfill;
-}
+global.atob ||= atobPolyfill;
 /* eslint-enable @silverhand/fp/no-mutation, @typescript-eslint/no-unnecessary-condition */
