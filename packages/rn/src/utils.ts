08abea379e7d5725bea38a6c70f400c6aef11877
Gao Sun <gao@silverhand.io>
2024-05-21 11:13:41 +0800

    refactor!: fix encoding issue (#27)

--- packages/rn/src/utils.ts
+++ packages/rn/src/utils.ts
@@ -1,12 +1,13 @@
 import * as Crypto from 'expo-crypto';
-import { encode, fromUint8Array, toUint8Array } from 'js-base64';
+import { fromUint8Array } from 'js-base64';
 
 export const generateRandomString = async (byteLength = 64) =>
   fromUint8Array(await Crypto.getRandomBytesAsync(byteLength), true);
 
 export const generateCodeChallenge = async (codeVerifier: string): Promise<string> => {
+  const encodedCodeVerifier = new TextEncoder().encode(codeVerifier);
   const codeChallenge = new Uint8Array(
-    await Crypto.digest(Crypto.CryptoDigestAlgorithm.SHA256, toUint8Array(encode(codeVerifier)))
+    await Crypto.digest(Crypto.CryptoDigestAlgorithm.SHA256, encodedCodeVerifier)
   );
   return fromUint8Array(codeChallenge, true);
 };
